069b531b4f32ebae279a269f23884f0243161bf5
Prabhu Kalyan Korivi <prabhukalyan30@gmail.com>
2024-03-19 23:23:02 +0530

    added tailwind to restaurant menu

--- tech-zone/src/components/Dish.js
+++ tech-zone/src/components/Dish.js
@@ -4,15 +4,15 @@ export default function Dish({ item }) {
     // console.log(item);
     const { name, price, description, imageId } = item;
     return (
-        <div className="item">
+        <div className="item pt-6 py-8 border border-dashed border-x-0 border-t-0 border-b-gray-400 flex justify-between">
             <div className="item-details">
-                <h1 className="item-name">{name}</h1>
-                <p className="item-price">₹ {price / 100}</p>
-                <p className="item-description">{description}</p>
+                <h1 className="item-name font-bold text-gray-800">{name}</h1>
+                <p className="item-price text-sm">₹ {price / 100}</p>
+                <p className="item-description pt-8 text-[0.825rem] text-gray-400 w-[80%]">{description}</p>
             </div>
-            <div className="item-image">
-                <img src={RESTAURANT_IMAGE + imageId} />
-                <button>ADD</button>
+            <div className="item-image flex flex-col gap-2 cursor-pointer">
+                <img src={RESTAURANT_IMAGE + imageId} className='w-[120px] h-[100px] rounded-lg bg-center bg-cover' />
+                <button className='px-8 py-1 font-bold text-sm text-green-500 bg-white rounded-sm border border-slate-300'>ADD</button>
             </div>
         </div>
     )
--- tech-zone/src/components/RestaurantMenu.js
+++ tech-zone/src/components/RestaurantMenu.js
@@ -22,9 +22,15 @@ export default function RestaurantMenu() {
     const restaurantInfo = restaurantDetails?.data?.cards[0]?.card?.card?.info;
     let recommendedItems = restaurantDetails?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
 
+    const allCategories = restaurantDetails?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+    const categories = allCategories.filter(category => category?.card?.card?.["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+    console.log(categories);
+
     if (!recommendedItems) {
       recommendedItems = restaurantDetails?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
     }
+
     const categoryHeading = restaurantDetails?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.title;
 
     // console.log(restaurantDetails);
@@ -41,36 +47,36 @@ export default function RestaurantMenu() {
   if (restaurantDetails === null) return <Shimmer />;
 
   return (
-    <div className="restaurant-page">
-      <div className="restaurant-menu">
-        <p className="bread-crumb">Home / {restaurantInfo?.city} / <span>{restaurantInfo?.name}</span></p>
+    <div className="restaurant-page  bg-gray-100">
+      <div className="restaurant-menu w-[800px] m-auto pt-8">
+        <p className="bread-crumb text-xs  text-gray-500"><span>Home</span> / {restaurantInfo?.city} / <span className="text-gray-900">{restaurantInfo?.name}</span></p>
 
-        <div className="res-header">
-          <div className="res-header-top">
+        <div className="res-header py-8 px-4 border border-dashed border-t-0 border-x-0 border-b-gray-400">
+          <div className="res-header-top flex justify-between">
             <div className="res-header-top-left">
-              <h1 className="res-name">{restaurantInfo?.name}</h1>
-              <p className="res-cuisines">{restaurantInfo?.cuisines.join(', ')}</p>
-              <p className="res-areaName">{restaurantInfo?.areaName}, {restaurantInfo?.sla.lastMileTravelString}</p>
+              <h1 className="res-name font-bold text-xl">{restaurantInfo?.name}</h1>
+              <p className="res-cuisines text-sm text-gray-500 pt-2">{restaurantInfo?.cuisines.join(', ')}</p>
+              <p className="res-areaName text-sm text-gray-500 pt-0">{restaurantInfo?.areaName}, {restaurantInfo?.sla.lastMileTravelString}</p>
             </div>
-            <div className="res-header-top-right">
-              <p className="res-rating"><i className="fa-sharp fa-solid fa-star"></i> {restaurantInfo?.avgRatingString}</p>
-              <p className="ratings-string">{restaurantInfo?.totalRatingsString}</p>
+            <div className="res-header-top-right border border-gray-400 rounded-lg text-green-500 text-center font-bold p-2">
+              <p className="res-rating  border border-t-0 border-x-0 border-b-gray-400 pb-2"><i className="fa-sharp fa-solid fa-star"></i> {restaurantInfo?.avgRatingString}</p>
+              <p className="ratings-string text-gray-400 text-xs pt-2">{restaurantInfo?.totalRatingsString}</p>
             </div>
           </div>
 
           <div>
-            <p className="res-distance-delivery-msg"> <i className="fa-solid fa-person-biking"></i> {restaurantInfo?.feeDetails?.message}</p>
+            <p className="res-distance-delivery-msg text-gray-500 text-sm pt-4"> <i className="fa-solid fa-person-biking"></i> {restaurantInfo?.feeDetails?.message}</p>
           </div>
         </div>
 
-        <div className="res-delivery-time-cost">
+        <div className="res-delivery-time-cost px-4 py-4 flex gap-6 text-gray-800">
           <p><i className="fa-solid fa-clock"></i> {restaurantInfo?.sla.slaString}</p>
           <p><i className="fa-solid fa-coins"></i> {restaurantInfo?.costForTwoMessage}</p>
         </div>
       </div>
 
-      <div>
-        <h1 className="categoryHeading">{categoryHeading} ({recommendedItems.length})</h1>
+      <div className="w-[800px] m-auto px-4 pt-4">
+        <h1 className="categoryHeading font-bold text-xl text-blue-800 pb-2">{categoryHeading} ({recommendedItems.length})</h1>
         {
           recommendedItems.map((item) => (
             <Dish key={item.card.info.id} item={item.card.info} />
